0012f63381f04bc6ef95992dd5a1dc7d30f2ff6b
Arran Dengate <arran.dengate@gmail.com>
2014-09-03 18:52:32 +1000

    Initial commit

---
+++ convolver.py
@@ -0,0 +1,63 @@
+__author__ = 'arran'
+
+import scipy as sp
+import math
+
+from scipy import ndimage
+
+def normalisekernel (anarray):
+    sum = 0
+    for x in range (0, anarray.shape[0]):
+        for y in range (0, anarray.shape[1]):
+            sum += anarray[x][y]
+
+    print('Sum was ' + str(sum))
+
+    for x in range (0, anarray.shape[0]):
+        for y in range (0, anarray.shape[1]):
+            anarray[x][y] = anarray[x][y] / sum
+
+    return anarray
+
+def makekernel(kernelsize):
+
+   if (kernelsize % 2) == 0:
+      raise Exception("When calling makekernel method, kernel size should not be even.")
+
+   half = (kernelsize-1)/2
+   kernel = sp.ones((kernelsize,kernelsize))
+   kernel[half][half] = 1
+   center = ((half),(half))
+
+   for x in range (0, kernel.shape[0]):
+      for y in range (0, kernel.shape[1]):
+         if (x,y) != center:
+            kernel[x][y] = (1 / (math.hypot((center[0] - x),(center[1]) - y) ** 2)) / 28
+
+   return kernel
+
+def convolve (original, kernel):
+
+   result = ndimage.convolve(original, kernel)
+   return result
+
+def main():
+
+   sp.set_printoptions(precision=2, suppress=True)
+
+   kernel = makekernel(9)
+
+   print(kernel)
+
+
+   testdata = sp.ones((14,14))
+   testdata[7][7] = 30
+   print(testdata)
+
+   result = ndimage.convolve(testdata, kernel)
+
+   sp.set_printoptions(precision=2)
+   print(result)
+
+if __name__ == '__main__':
+   main()
---
+++ main.py
@@ -0,0 +1,42 @@
+__author__ = 'arran'
+from PIL import Image
+from PIL import ImageFilter
+from PIL import ImageEnhance
+im = Image.open("assets/australia_night_201204-10_lrg-cropped.jpg")
+import numpy as np
+import scipy as sp
+import convolver
+
+original_image = im.copy()
+original_image = original_image.convert("L")
+
+im = im.convert("L")
+
+original_image.save('original_image.jpg')
+
+imagewidth = im.size[0]
+imageheight = im.size[1]
+
+imagedata = sp.zeros((imagewidth, imageheight))
+
+print(im.getextrema())
+
+for x in range (0,imagewidth):
+   for y in range (0, imageheight):
+      imagedata[x][y] = im.getpixel((x,y))
+
+print(imagedata)
+
+print('Starting convolve...')
+imagedata = convolver.convolve(imagedata, convolver.makekernel(151))
+print('Completed convolve.')
+
+final_image = Image.fromarray(imagedata)
+
+final_image = final_image.rotate(angle=-90)
+final_image = final_image.transpose(Image.FLIP_LEFT_RIGHT)
+
+final_image = final_image.convert("RGB")
+#enhancer = ImageEnhance.Brightness(final_image)
+#final_image = enhancer.enhance(4.0)
+final_image.save('final_image.jpg')
